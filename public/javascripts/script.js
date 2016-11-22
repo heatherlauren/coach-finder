efd99af74a66f220192288653c949ff07dcd6656
heatherlauren <heatherlaurendavidson@gmail.com>
2016-11-22 15:04:20 +0000

    generate map and markers

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,29 @@
-// Retrieve coach station data
-
-$.getJSON('//data.gov.uk/data/api/service/transport/naptan_coach_stations/postcode?postcode=EC2A 3LT&distance=3', function (data) {
-  var stationNames = [];
-  $.each(data.result, function (index) {
-    stationNames.push(data.result[index].name);
-  })
-  console.log(stationNames)
-})
-
-
-// Initialise map
-
+// Generate map and markers
 
 function initMap() {
   var map;
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.522647, lng: -0.080758},
-    zoom: 15
+    center: {lat: 51.525325, lng: -0.080969},
+    zoom: 14
+  });
+
+  var precedentMarker = new google.maps.Marker({
+    position: {lat: 51.525325, lng: -0.080969},
+    map: map
+  });
+
+  $.getJSON('//data.gov.uk/data/api/service/transport/naptan_coach_stations/postcode?postcode=EC2A 3LT&distance=3', function (data) {
+
+    var stations = data.result;
+
+    $.each(stations, function (index, value) {
+      var coords = value.latlong.coordinates;
+      var latLng = new google.maps.LatLng(coords[1], coords[0])
+      var stationMarker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
+    });
   });
 }
