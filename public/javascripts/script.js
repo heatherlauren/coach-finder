0d019ff3bafeaea2032dccf89d86f1e3e1472600
heatherlauren <heatherlaurendavidson@gmail.com>
2016-11-22 19:48:17 +0000

    add snazzymaps styling

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,7 +6,51 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 51.525325, lng: -0.080969},
-    zoom: 14
+    zoom: 14,
+    styles: [
+      {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": 100
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "on"
+              },
+              {
+                  "color": "#C6E2FF"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#C5E3BF"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#D1D1B8"
+              }
+          ]
+      }
+    ]
   });
 
   var precedentMarker = new google.maps.Marker({
